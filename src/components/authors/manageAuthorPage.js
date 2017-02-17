7f72a3757733a345c800ee6c8a9437dedb6587c6
Jessie Samuelson <jessie@mediabrix.com>
2017-02-17 10:19:46 -0500

    saves authors

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,12 +1,20 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var toastr = require('toastr');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+  mixins: [
+		Router.Navigation
+	],
+
   getInitialState: function() {
     return {
-      author: { id: '', firstName: '', lastName: '' }
+      author: { id: '', firstName: '', lastName: '' },
+      errors: {}
     };
   },
 
@@ -17,10 +25,46 @@ var ManageAuthorPage = React.createClass({
     return this.setState({author: this.state.author});
   },
 
+  authorFormIsValid: function() {
+    var formIsValid = true;
+    this.state.errors = {};
+
+    if (this.state.author.firstName.length < 3) {
+      this.state.errors.firstName = 'First name must be at least 3 characters.';
+      formIsValid = false;
+    }
+
+    if (this.state.author.lastName.length < 3) {
+      this.state.errors.lastName = 'Last name must be at least 3 characters.';
+      formIsValid = false;
+    }
+
+    this.setState({errors: this.state.errors});
+    console.log('formIsValid ~~>', formIsValid);
+    return formIsValid;
+
+  }
+
+  saveAuthor: function(event) {
+		event.preventDefault();
+
+    if (!this.authorFormIsValid()) {
+      return;
+    }
+		AuthorApi.saveAuthor(this.state.author);
+    toastr.success('Author Saved');
+		this.transitionTo('authors');
+	},
+
 	render: function() {
 		return (
 			<div>
-				<AuthorForm author={this.state.author} onChange={this.setAuthorState}/>
+				<AuthorForm
+          author={this.state.author}
+          onChange={this.setAuthorState}
+          onSave={this.saveAuthor}
+          errors={this.state.errors}
+        />
 			</div>
 		);
 	}
